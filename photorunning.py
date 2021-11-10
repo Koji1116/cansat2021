7eabaf72003ecfc6a3ba1a16263953759896a7fc
yoppi <oosima@kimura-lab.net>
2021-11-10 14:33:07 +0900

    error

--- photorunning.py
+++ photorunning.py
@@ -157,6 +157,7 @@ def adjustment_mag(strength, t, magx_off, magy_off):
         mag_x_old = mag_x
         mag_y_old = mag_y
         theta_old = calibration.angle(mag_x_old, mag_y_old, magx_off, magy_off)
+    print("123")
     motor.deceleration(strength_l, strength_r)
 
 
