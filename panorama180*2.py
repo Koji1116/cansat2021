94e5e13ac33d90af8ea6fb0ad856a835e7d50019
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-11-15 14:17:34 +0900

    panorama180*2
    パノラマを180度ずつ作成したものを合成して360度にする

--- panorama180*2.py
+++ panorama180*2.py
@@ -28,65 +28,121 @@ def shooting_angle(theta, path_src_panorama, dict_angle, wid, hig):
     dict_angle1 = dict_angle[0]
     dict_angle2 = dict_angle[1]
     dict_angle3 = dict_angle[2]
+    dict_angle4 = dict_angle[3]
+    dict_angle5 = dict_angle[4]
+    dict_angle6 = dict_angle[5]
+
     # Unpack the path_src_panorama
     path_src_panorama1 = path_src_panorama[0]
     path_src_panorama2 = path_src_panorama[1]
     path_src_panorama3 = path_src_panorama[2]
+    path_src_panorama4 = path_src_panorama[3]
+    path_src_panorama5 = path_src_panorama[4]
+    path_src_panorama6 = path_src_panorama[5]
+
 
     if switch:
-        for i in range(9):
+        for i in range(6):
             if 30 * i <= theta and theta <= 10 + 30 * i and not dict_angle1[i + 1]:
                 take.picture(path_src_panorama1, wid, hig)
                 dict_angle1[i + 1] = True
                 switch = False
                 break
     if switch:
-        for i in range(9):
+        for i in range(6):
             if 10 + 30 * i <= theta and theta <= 20 + 30 * i and not dict_angle2[i + 1]:
                 take.picture(path_src_panorama2, wid, hig)
                 dict_angle2[i + 1] = True
                 switch = False
                 break
     if switch:
-        for i in range(9):
+        for i in range(6):
             if 20 + 30 * i <= theta and theta <= 30 + 30 * i and not dict_angle3[i + 1]:
                 take.picture(path_src_panorama3, wid, hig)
                 dict_angle3[i + 1] = True
+                switch = False
                 break
-    return [dict_angle1, dict_angle2, dict_angle3]
+    if switch:
+        for i in range(6):
+            if 30 * (i+5) <= theta and theta <= 10 + 30 * (i+5) and not dict_angle4[i + 1]:
+                take.picture(path_src_panorama4, wid, hig)
+                dict_angle4[i + 1] = True
+                switch = False
+                break
+    if switch:
+        for i in range(6):
+            if 10 + 30 * (i+5) <= theta and theta <= 20 + 30 * (i+5) and not dict_angle5[i + 1]:
+                take.picture(path_src_panorama5, wid, hig)
+                dict_angle5[i + 1] = True
+                switch = False
+                break
+    if switch:
+        for i in range(6):
+            if 20 + 30 * (i+5) <= theta and theta <= 30 + 30 * (i+5) and not dict_angle6[i + 1]:
+                take.picture(path_src_panorama6, wid, hig)
+                dict_angle6[i + 1] = True
+                switch = False
+                break
+    return [dict_angle1, dict_angle2, dict_angle3, dict_angle4, dict_angle5, dict_angle6]
 
 
 def check(dict_angle, path_src_panorama):
     """
-    12枚の写真が撮影されたかを判断するための関数
+    6枚の写真が撮影されたかを判断するための関数
     shooting内で使用
     戻り値はsrcdir
-    12枚の写真が撮影された場合は，該当するパス(''じゃない)を返す。elseの場合は''を返す。
+    6枚の写真が撮影された場合は，該当するパス(''じゃない)を返す。elseの場合は''を返す。
     """
     srcdir = ''
     # Unpack the dict_angle
     dict_angle1 = dict_angle[0]
     dict_angle2 = dict_angle[1]
     dict_angle3 = dict_angle[2]
+    dict_angle4 = dict_angle[3]
+    dict_angle5 = dict_angle[4]
+    dict_angle6 = dict_angle[5]
+
     # Unpack the path_src_panorama
     path_src_panorama1 = path_src_panorama[0]
     path_src_panorama2 = path_src_panorama[1]
     path_src_panorama3 = path_src_panorama[2]
+    path_src_panorama4 = path_src_panorama[3]
+    path_src_panorama5 = path_src_panorama[4]
+    path_src_panorama6 = path_src_panorama[5]
+
     # Count the number of photos stored in each directory
     number_photos1 = list(dict_angle1.values()).count(True)
     number_photos2 = list(dict_angle2.values()).count(True)
     number_photos3 = list(dict_angle3.values()).count(True)
+    number_photos4 = list(dict_angle4.values()).count(True)
+    number_photos5 = list(dict_angle5.values()).count(True)
+    number_photos6 = list(dict_angle6.values()).count(True)
+
     # Print number_photos
     print_xbee(f'number_photo1:\t{number_photos1}')
     print_xbee(f'number_photo2:\t{number_photos2}')
     print_xbee(f'number_photo3:\t{number_photos3}')
+    print_xbee(f'number_photo4:\t{number_photos4}')
+    print_xbee(f'number_photo5:\t{number_photos5}')
+    print_xbee(f'number_photo6:\t{number_photos6}')
+
     # Find a directory/file_name with 12 photos
-    srcdir = path_src_panorama1 if number_photos1 == 12 else srcdir
-    srcdir = path_src_panorama2 if number_photos2 == 12 else srcdir
-    srcdir = path_src_panorama3 if number_photos3 == 12 else srcdir
+    srcdir1 = path_src_panorama1 if number_photos1 == 6 else srcdir
+    srcdir1 = path_src_panorama2 if number_photos2 == 6 else srcdir
+    srcdir1 = path_src_panorama3 if number_photos3 == 6 else srcdir
+    srcdir2 = path_src_panorama4 if number_photos4 == 6 else srcdir
+    srcdir2 = path_src_panorama5 if number_photos5 == 6 else srcdir
+    srcdir2 = path_src_panorama6 if number_photos6 == 6 else srcdir
+
     # Get the directory name
-    rfd = srcdir.rfind('/')
-    srcdir = srcdir[:rfd]
+    rfd1 = srcdir1.rfind('/')
+    srcdir1 = srcdir1[:rfd1]
+
+    rfd2 = srcdir1.rfind('/')
+    srcdir2 = srcdir1[:rfd2]
+
+    if srcdir1 and srcdir2:
+        srcdir = [srcdir1, srcdir2]
     return srcdir
 
 
@@ -101,6 +157,10 @@ def initialize(path_src_panorama):
     path_src_panorama1 = path_src_panorama[0]
     path_src_panorama2 = path_src_panorama[1]
     path_src_panorama3 = path_src_panorama[2]
+    path_src_panorama4 = path_src_panorama[3]
+    path_src_panorama5 = path_src_panorama[4]
+    path_src_panorama6 = path_src_panorama[5]
+
     # Initialize the directory 1
     rfd1 = path_src_panorama1.rfind('/')
     dir_src_panorama1 = path_src_panorama1[:rfd1]
@@ -116,13 +176,32 @@ def initialize(path_src_panorama):
     dir_src_panorama3 = path_src_panorama3[:rfd3]
     shutil.rmtree(dir_src_panorama3)
     os.mkdir(dir_src_panorama3)
+    # Initialize the directory 4
+    rfd4 = path_src_panorama4.rfind('/')
+    dir_src_panorama4 = path_src_panorama4[:rfd4]
+    shutil.rmtree(dir_src_panorama4)
+    os.mkdir(dir_src_panorama4)
+    # Initialize the directory 5
+    rfd5 = path_src_panorama5.rfind('/')
+    dir_src_panorama5 = path_src_panorama5[:rfd5]
+    shutil.rmtree(dir_src_panorama5)
+    os.mkdir(dir_src_panorama5)
+    # Initialize the directory 6
+    rfd6 = path_src_panorama6.rfind('/')
+    dir_src_panorama6 = path_src_panorama6[:rfd6]
+    shutil.rmtree(dir_src_panorama6)
+    os.mkdir(dir_src_panorama6)
+
     # Initializing variables
     count_panorama = 0
     count_stuck = 0
-    dict_angle1 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11: False, 12: False}
-    dict_angle2 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11: False, 12: False}
-    dict_angle3 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11: False, 12: False}
-    dict_angle = [dict_angle1, dict_angle2, dict_angle3]
+    dict_angle1 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False}
+    dict_angle2 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False}
+    dict_angle3 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False}
+    dict_angle4 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False}
+    dict_angle5 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False}
+    dict_angle6 = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False}
+    dict_angle = [dict_angle1, dict_angle2, dict_angle3, dict_angle4, dict_angle5, dict_angle6]
     return count_panorama, count_stuck, dict_angle
 
 def azimuth(magx_off, magy_off, n=1):
@@ -209,7 +288,7 @@ def shooting(t_rotation_pano, mag_mat, path_src_panorama, path_paradete, log_pan
         print_xbee('\n')
         print_xbee('\n')
         other.log(log_panoramashooting, datetime.datetime.now(), sumθ, latestθ, preθ2, deltaθ)
-        time.sleep(0.2)
+        time.sleep(0.1)
     return srcdir
 
 
@@ -223,7 +302,7 @@ def composition(srcdir, srcext='.jpg', dstext='.jpg'):
     dstext:パノラマ写真の拡張子
     """
     srcfilecount = len(glob.glob1(srcdir, '*' + srcext))
-    resultcount = len(glob.glob1('/home/pi/Desktop/Cansat2021ver/dst_panorama', '*' + dstext))
+    resultcount = len(glob.glob1('/home/pi/Desktop/cansat2021/dst_panorama1', '*' + dstext))
     print_xbee(f'srcfilecount:\t{srcfilecount}')
     print_xbee(f'resultcount:\t{resultcount}')
 
@@ -244,7 +323,7 @@ def composition(srcdir, srcext='.jpg', dstext='.jpg'):
 
     else:
         print_xbee('##--Composition failed--##')
-    path_dst = other.filename('/home/pi/Desktop/cansat2021/dst_panorama/dst', 'jpg')
+    path_dst = other.filename('/home/pi/Desktop/cansat2021/dst_panorama1/panoramaShooting', 'jpg')
     cv2.imwrite(path_dst, result)
     print_xbee('###################################################################')
     print_xbee(f'Panorama name:\t{path_dst}')
@@ -252,26 +331,86 @@ def composition(srcdir, srcext='.jpg', dstext='.jpg'):
     return path_dst
 
 
+def composition2(srcdir, srcext='.jpg', dstext='.jpg'):
+    """
+    パノラマを合成するための関数
+    ソースディレクトリ内に合成用の写真を番号をつけて入れておく。（例：IMG0.jpg,IMG1.jpg）
+    宛先ディレクトリ内で番号(0~).(拡張子)の形でパノラマ写真が保存される。
+    srcdir:ソースディレクトリ
+    srcext:ソースの拡張子
+    dstext:パノラマ写真の拡張子
+    """
+    srcfilecount = len(glob.glob1(srcdir, '*' + srcext))
+    resultcount = len(glob.glob1('/home/pi/Desktop/cansat2021/dst_panorama', '*' + dstext))
+    print_xbee(f'srcfilecount:\t{srcfilecount}')
+    print_xbee(f'resultcount:\t{resultcount}')
+
+    photos = []
+
+    for i in range(0, srcfilecount):
+        if len(str(i)) == 1:
+            photos.append(cv2.imread(srcdir + '/panoramaShooting000' + str(i) + srcext))
+        else:
+            photos.append(cv2.imread(srcdir + '/panoramaShooting00' + str(i) + srcext))
+
+    print_xbee(f'Imreaded photo:\t{len(photos)}')
+
+    stitcher = cv2.Stitcher.create()
+    status, result = stitcher.stitch(photos)
+    if status == 0:
+        print_xbee('##--Composition succeed--##')
+
+    else:
+        print_xbee('##--Composition failed--##')
+    path_dst = other.filename('/home/pi/Desktop/cansat2021/dst_panorama2/dst', 'jpg')
+    cv2.imwrite(path_dst, result)
+    print_xbee('###################################################################')
+    print_xbee(f'Panorama name:\t{path_dst}')
+    print_xbee('###################################################################')
+    return path_dst
+
 if __name__ == "__main__":
     # Initialization
     bmc050.bmc050_setup()
     motor.setup()
     xbee.on()
-    path_src_panorama1 = '/home/pi/Desktop/Cansat2021ver/src_panorama1/panoramaShooting'
-    path_src_panorama2 = '/home/pi/Desktop/Cansat2021ver/src_panorama2/panoramaShooting'
-    path_src_panorama3 = '/home/pi/Desktop/Cansat2021ver/src_panorama3/panoramaShooting'
-    path_src_panorama = (path_src_panorama1, path_src_panorama2, path_src_panorama3)
-    path_dst_panoraam = '/home/pi/Desktop/Cansat2021ver/dst_panorama'
-    path_paradete = '/home/pi/Desktop/Cansat2021ver/photostorage/paradete'
-    log_panoramashooting = other.filename('/home/pi/Desktop/Cansat2021ver/log/panoramaLog', 'txt')
+    path_src_panorama1 = '/home/pi/Desktop/cansat2021/src_panorama1/panoramaShooting'
+    path_src_panorama2 = '/home/pi/Desktop/cansat2021/src_panorama2/panoramaShooting'
+    path_src_panorama3 = '/home/pi/Desktop/cansat2021/src_panorama3/panoramaShooting'
+    path_src_panorama4 = '/home/pi/Desktop/cansat2021/src_panorama4/panoramaShooting'
+    path_src_panorama5 = '/home/pi/Desktop/cansat2021/src_panorama5/panoramaShooting'
+    path_src_panorama6 = '/home/pi/Desktop/cansat2021/src_panorama6/panoramaShooting'
+    
+    path_src_panorama = (path_src_panorama1, path_src_panorama2, path_src_panorama3,
+                        path_src_panorama4, path_src_panorama5, path_src_panorama6)
+    path_dst_panorama = '/home/pi/Desktop/cansat2021/dst_panorama'
+    path_paradete = '/home/pi/Desktop/cansat2021/photostorage/paradete'
+    log_panoramashooting = other.filename('/home/pi/Desktop/cansat2021/log/panoramaLog', 'txt')
+    
+    for i in range(1, 7):
+        other.make_dir("path_src_panorama"+str(i))
 
-    mag_mat = calibration.magdata_matrix(40, -40, 60)
+    other.make_dir(path_dst_panorama)
+    mag_mat = calibration.magdata_matrix(40, -40, 30)
     t_rotation_pano = 0.1
     t_start = time.time()
     srcdir = shooting(t_rotation_pano, mag_mat, path_src_panorama, path_paradete, log_panoramashooting)
     print_xbee(time.time() - t_start)
     if input('Composition y/n \t') == 'y':
-        t_start = time.time()  # プログラムの開始時刻
-        composition(srcdir)
-        runTime = time.time() - t_start
-        print_xbee(runTime)
+        t_start1 = time.time()  # プログラムの開始時刻
+        composition(srcdir[0])
+        runTime1 = time.time() - t_start1
+        print_xbee(f'runTime1 :\t{runTime1}')
+
+        t_start2 = time.time()
+        composition(srcdir[1])
+        runTime2 = time.time() - t_start2
+        print_xbee(f'runTime2 :\t{runTime2}')
+        print_xbee('\n')
+        print_xbee(f'runTime :\t{time.time() - t_start1}')
+
+        composition2('/home/pi/Desktop/cansat2021/dst_panorama1/panoramaShooting')
+
+
+
+
